5bbd6d5f4a8d9095469cb0876ccd98e6d034773e
Alexandros Schillings <aschillings@gmail.com>
2015-03-22 23:01:03 +0000

    Updated copyright notice

--- resourcemirror/src/main/java/uk/co/alt236/resourcemirror/reflectors/Mirror.java
+++ resourcemirror/src/main/java/uk/co/alt236/resourcemirror/reflectors/Mirror.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Alexandros Schillings
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.co.alt236.resourcemirror.reflectors;
 
 import android.content.Context;
@@ -9,24 +25,6 @@ import java.util.Map;
 import uk.co.alt236.resourcemirror.reflectors.base.AbstractResourceReflector;
 import uk.co.alt236.resourcemirror.reflectors.base.ResourceReflector;
 import uk.co.alt236.resourcemirror.util.ResourceType;
-
-/**
- * ****************************************************************************
- * Copyright 2013 Alexandros Schillings
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance of the License. You may obtain a copy of
- * the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed of an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- * ****************************************************************************
- */
 
 public class Mirror {
     private static final Object MAP_LOCK = new Object();
