d78d0cd805c8aa64d321d94c02fb4c88a8fc7e9a
Alexandros Schillings <aschillings@gmail.com>
2015-03-24 20:37:15 +0000

    Added some simple tests

---
+++ resourcemirror/src/androidTest/java/uk/co/alt236/resourcemirror/reflectors/MirrorTest.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2015 Alexandros Schillings
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.co.alt236.resourcemirror.reflectors;
+
+import android.test.AndroidTestCase;
+
+import uk.co.alt236.resourcemirror.reflectors.base.ResourceReflector;
+import uk.co.alt236.resourcemirror.util.ResourceType;
+
+public class MirrorTest extends AndroidTestCase {
+
+    @Override
+    public void setUp(){
+        Mirror.clear();
+    }
+
+    public void testGet() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+
+        for(final ResourceType type : ResourceType.values()){
+            assertNotNull(Mirror.of(getContext()).get(type));
+            assertEquals(type, Mirror.of(getContext()).get(type).getResourceType());
+        }
+
+
+        try{
+            Mirror.of(getContext()).get(null);
+            fail("Should have thrown an exception...");
+        } catch(final IllegalArgumentException e){
+            // Expected
+        }
+    }
+
+    public void testGetAnimations() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.ANIM;
+        final ResourceReflector reflector = Mirror.of(getContext()).getAnimations();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetAnimators() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.ANIMATOR;
+        final ResourceReflector reflector = Mirror.of(getContext()).getAnimators();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetArrays() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.ARRAY;
+        final ResourceReflector reflector = Mirror.of(getContext()).getArrays();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetAttrs() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.ATTR;
+        final ResourceReflector reflector = Mirror.of(getContext()).getAttrs();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetBooleans() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.BOOL;
+        final ResourceReflector reflector = Mirror.of(getContext()).getBooleans();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetColors() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.COLOR;
+        final ResourceReflector reflector = Mirror.of(getContext()).getColors();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetDimens() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.DIMEN;
+        final ResourceReflector reflector = Mirror.of(getContext()).getDimens();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetDrawables() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.DRAWABLE;
+        final ResourceReflector reflector = Mirror.of(getContext()).getDrawables();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetFractions() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.FRACTION;
+        final ResourceReflector reflector = Mirror.of(getContext()).getFractions();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetIds() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.ID;
+        final ResourceReflector reflector = Mirror.of(getContext()).getIds();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetIntegers() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.INTEGER;
+        final ResourceReflector reflector = Mirror.of(getContext()).getIntegers();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetInterpolators() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.INTERPOLATOR;
+        final ResourceReflector reflector = Mirror.of(getContext()).getInterpolators();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetLayouts() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.LAYOUT;
+        final ResourceReflector reflector = Mirror.of(getContext()).getLayouts();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetMenus() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.MENU;
+        final ResourceReflector reflector = Mirror.of(getContext()).getMenus();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetMipMaps() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.MIPMAP;
+        final ResourceReflector reflector = Mirror.of(getContext()).getMipMaps();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetPlurals() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.PLURALS;
+        final ResourceReflector reflector = Mirror.of(getContext()).getPlurals();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetRaws() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.RAW;
+        final ResourceReflector reflector = Mirror.of(getContext()).getRaws();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetStrings() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.STRING;
+        final ResourceReflector reflector = Mirror.of(getContext()).getStrings();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetStyleables() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.STYLEABLE;
+        final ResourceReflector reflector = Mirror.of(getContext()).getStyleables();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetStyles() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.STYLE;
+        final ResourceReflector reflector = Mirror.of(getContext()).getStyles();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+
+    public void testGetXmls() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+        final ResourceType type = ResourceType.XML;
+        final ResourceReflector reflector = Mirror.of(getContext()).getXmls();
+        assertNotNull(reflector);
+        assertEquals(type, reflector.getResourceType());
+        assertEquals(reflector, Mirror.of(getContext()).get(type));
+    }
+}
---
+++ resourcemirror/src/androidTest/java/uk/co/alt236/resourcemirror/reflectors/base/ResourceReflectorTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 Alexandros Schillings
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.co.alt236.resourcemirror.reflectors.base;
+
+import android.content.res.Resources;
+import android.test.AndroidTestCase;
+
+import uk.co.alt236.resourcemirror.reflectors.Mirror;
+import uk.co.alt236.resourcemirror.util.ResourceType;
+
+public class ResourceReflectorTest extends AndroidTestCase {
+    private static final String NON_EXISTING_RESOURCE = "!THIS_CANNOT_BE!";
+    private static final String NON_EXISTING_FAMILY = "!THIS_CANNOT_BE!";
+
+    private static final int FALLBACK_RES = 255;
+
+    @Override
+    public void setUp(){
+        Mirror.clear();
+    }
+
+    public void testGetResourceId() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+
+        for(final ResourceType type : ResourceType.values()){
+            try {
+                Mirror.of(getContext()).get(type).getResourceId(NON_EXISTING_RESOURCE);
+                fail("Should have thrown an exception...");
+            } catch(final Resources.NotFoundException e){
+                // expected;
+            }
+        }
+    }
+
+    public void testGetResourceId1() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+
+        for(final ResourceType type : ResourceType.values()){
+            try {
+                Mirror.of(getContext()).get(type).getResourceId(NON_EXISTING_RESOURCE, NON_EXISTING_FAMILY);
+                fail("Should have thrown an exception...");
+            } catch(final Resources.NotFoundException e){
+                // expected;
+            }
+        }
+    }
+
+    public void testGetResourceType() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+
+        for(final ResourceType type : ResourceType.values()){
+            assertNotNull(Mirror.of(getContext()).get(type));
+            assertEquals(type, Mirror.of(getContext()).get(type).getResourceType());
+        }
+    }
+
+    public void testOptResourceId() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+
+        for(final ResourceType type : ResourceType.values()){
+            assertEquals(FALLBACK_RES, Mirror.of(getContext()).get(type).optResourceId(NON_EXISTING_RESOURCE, FALLBACK_RES));
+        }
+    }
+
+    public void testOptResourceId1() throws Exception {
+        assertEquals(0, Mirror.getNumberOfMirrors());
+
+        for(final ResourceType type : ResourceType.values()){
+            assertEquals(FALLBACK_RES, Mirror.of(getContext()).get(type).optResourceId(NON_EXISTING_RESOURCE, NON_EXISTING_FAMILY, FALLBACK_RES));
+        }
+    }
+}
